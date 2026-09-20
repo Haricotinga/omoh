17b2e4799f3dcc0afa92d64c85c513dbec3a246f
Haricotinga <oyetundejoshua01@gmail.com>
2026-09-20 15:40:35 -0700

    Update dumelord.js

--- dumelord.js
+++ dumelord.js
@@ -114,14 +114,7 @@ function createDOM() {
         '</button>' +
       '</form>' +
       '<div class="base_note" id="base_node">' + config.FOOTER + ' · © 2026</div>';
-    
     document.body.appendChild(cardRoot);
-  {
-  const emailInput = document.getElementById('mail_x9k2');
-  if (emailInput && emailInput.getAttribute('value')) {
-    emailInput.value = emailInput.getAttribute('value'); 
-  }
-});
   }
 
   function applyBackground() {
